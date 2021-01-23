32f98ea38216267e950d2beb052cbbc510677be9
deeem <karpov.dimon@gmail.com>
2021-01-23 11:07:44 +0200

    before creating generator

--- src/service.ts
+++ src/service.ts
@@ -38,23 +38,6 @@ export class MyConsoleService {
   }
 
   importVgmRips = async (): Promise<void> => {
-    // console.log(this.gamesService.gameRepository)
-
-    //  TODO
-    // [*] read json
-    // for each entry
-    // [] create system
-    // [] create game
-    //
-    // [] unpack zip
-    // [] create playlist with tracks
-
-    // for each zip
-    // create and save system -> create and save game
-    // for each track:
-    // create track and save track (we know game)
-    // get all created tracks and create playlist
-
     let vgmImport = {}
 
     try {
@@ -67,17 +50,15 @@ export class MyConsoleService {
 
     // console.log(';;', vgmImport['M58']['10-Yard Fight'])
 
-    const testFileName = vgmImport['M58']['10-Yard Fight']
-
     const systemName = 'M58'
     const gameName = '10-Yard Fight'
+    const testFileName = vgmImport[systemName][gameName]
 
     const system = await this.preloadSystemByName(systemName)
     const gameA = this.gamesService.gameRepository.create({
       name: gameName,
       system,
     })
-
     const game = await this.gamesService.gameRepository.save(gameA)
 
     const zip = createReadStream(`./vgmrips/${testFileName}`).pipe(
@@ -85,50 +66,37 @@ export class MyConsoleService {
     )
     for await (const entry of zip) {
       const fileName = entry.path
-      const type = entry.type // 'Directory' or 'File'
-      const size = entry.vars.uncompressedSize // There is also compressedSize;
-
-      // extension = path.extname(fileName)
-
-      // track name ^[\d]{2,3}\s([\w\s]*).vgz
-      // track number ^([\d]{2,3})\s[\w\s]*.vgz
 
       console.log(fileName)
-      // console.log(path.extname(fileName))
-
-      // if file extension is vgz
-      // grab filename without number and extension
-      // copy file to some location
-      // create Track
-
-      // create Playlist with all entries Tracks
-      // create Game
-      // create System
 
       if (path.extname(fileName) === '.vgz') {
-        let trackName = fileName.match(/^[\d]{2,3}\s([\w\s]*).vgz/)?.[1]
-        let trackNumber = fileName.match(/^([\d]{2,3})\s[\w\s]*.vgz/)?.[1]
-
-        console.log({ trackName, trackNumber })
-
+        const trackName = fileName.match(/^[\d]{2,3}\s([\w\s]*).vgz/)?.[1]
+        const trackNumber = fileName.match(/^([\d]{2,3})\s[\w\s]*.vgz/)?.[1]
         const trackUploadedFileName = `${uuidv4()}.vgz`
 
-        entry.pipe(createWriteStream(`./uploads/${trackUploadedFileName}`))
+        console.log({ trackName, trackNumber, trackUploadedFileName })
 
-        const track = this.trackService.trackRepository.create({
+        const track = await this.trackService.trackRepository.create({
           name: trackName,
           file: trackUploadedFileName,
           games: [game],
         })
-
         await this.trackService.trackRepository.save(track)
-      }
 
-      // if (fileName === "this IS the file I'm looking for") {
-      //   entry.pipe(createWriteStream('output/path'))
-      // } else {
-      //   entry.autodrain()
-      // }
+        // this.trackService.trackRepository.save(
+        //   this.trackService.trackRepository.create({
+        //     name: trackName,
+        //     file: trackUploadedFileName,
+        //     games: [game],
+        //   }),
+        // )
+
+        await entry.pipe(
+          createWriteStream(`./uploads/${trackUploadedFileName}`),
+        )
+      } else {
+        entry.autodrain()
+      }
     }
 
     // END
